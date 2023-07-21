fb5d7f9256211d3529937706b03ff08d84f5c256
Juan Martinez <juanmartinez@Juans-MacBook.local>
2023-07-21 15:56:17 -0400

    I added many files to the pages folder and connected all of my routes

---
+++ portfolio/src/pages/Portfolio.jsx
@@ -0,0 +1,8 @@
+
+export default function Portfolio (){
+    return(
+        <div>
+            <h1>This is the portfolio route</h1>
+        </div>
+    )
+}
---
+++ portfolio/src/pages/Resume.jsx
@@ -0,0 +1,8 @@
+export default function Resume(){
+    return (
+        <div>
+            <h1>This is the resume Route
+            </h1>
+        </div>
+    )
+}
