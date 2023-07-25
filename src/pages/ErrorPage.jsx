08367b5d04689793b38a5c756db49315f817ca08
Juan Martinez <juanmartinez@Juans-MacBook.local>
2023-07-25 04:06:31 -0400

    added link

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,8 +1,9 @@
-
+import { Link } from "react-router-dom"
 export default function ErrorPage(){
     return (
         <div>
             <h1>Oops there was an error!</h1>
+            <Linkink to="/">Home</Linkink>
         </div>
     )
 }
