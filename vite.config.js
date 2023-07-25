cb9947282694c616250167f9677645d12edbc52d
Juan Martinez <juanmartinez@Juans-MacBook.local>
2023-07-25 01:46:49 -0400

    deploid stack

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    port:3000,
-    open:true
-  }
-})
+  server: {
+    port: 3000,
+    open: true,
+  },
+  build: {
+    outDir: 'dist', // Specify the output directory for the production build
+  },
+});
