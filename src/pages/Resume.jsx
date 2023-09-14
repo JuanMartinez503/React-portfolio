20e66cf8ef513df7f2d7e9b126a43e47637e0d04
Juan Martinez <juanmartinez@Juans-MacBook.local>
2023-09-14 13:41:58 -0400

    added changes to the icons by wrapping them in a div

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -19,83 +19,56 @@ export default function Resume() {
         <div className="m-1 fronted ">
           <h3 className="d-flex">Front-End:</h3>
           <div className="home-skills-icon-cont d-flex flex-wrap text-center justify-content-between">
-            <div className="dev-icon-div">
+            <div className="dev-icon-div" data-aos="fade-right">
               {" "}
-              <i
-                className="devicon-html5-plain colored skill-icon"
-                data-aos="fade-right"
-              >
+              <i className="devicon-html5-plain colored skill-icon">
                 <p>HTML</p>
               </i>{" "}
             </div>
-            <div className="dev-icon-div">
+            <div className="dev-icon-div" data-aos="fade-left">
               {" "}
-              <i
-                className="devicon-css3-plain colored skill-icon"
-                data-aos="fade-left"
-              >
+              <i className="devicon-css3-plain colored skill-icon">
                 <p>CSS</p>
               </i>
             </div>
-            <div className="dev-icon-div">
+            <div className="dev-icon-div" data-aos="fade-right">
               {" "}
-              <i
-                className="devicon-javascript-plain colored skill-icon"
-                data-aos="fade-right"
-              >
+              <i className="devicon-javascript-plain colored skill-icon">
                 <p>Javascript</p>
               </i>
             </div>
-            <div className="dev-icon-div">
+            <div className="dev-icon-div" data-aos="fade-left">
               {" "}
-              <i
-                className="devicon-react-original colored skill-icon"
-                data-aos="fade-left"
-              >
+              <i className="devicon-react-original colored skill-icon">
                 <p>React</p>
               </i>
             </div>
-            <div className="dev-icon-div">
+            <div className="dev-icon-div" data-aos="fade-right">
               {" "}
-              <i
-                className="devicon-jquery-plain colored skill-icon"
-                data-aos="fade-right"
-              >
+              <i className="devicon-jquery-plain colored skill-icon">
                 <p>JQuery</p>
               </i>
             </div>
-            <div className="dev-icon-div">
-              <i
-                className="devicon-bootstrap-plain colored skill-icon"
-                data-aos="fade-left"
-              >
+            <div className="dev-icon-div" data-aos="fade-left">
+              <i className="devicon-bootstrap-plain colored skill-icon">
                 <p>Bootstrap</p>
               </i>
             </div>
-            <div className="dev-icon-div">
+            <div className="dev-icon-div" data-aos="fade-right">
               {" "}
-              <i
-                className="devicon-handlebars-plain colored skill-icon"
-                data-aos="fade-right"
-              >
+              <i className="devicon-handlebars-plain colored skill-icon">
                 <p>Handlebars</p>
               </i>
             </div>
-            <div className="dev-icon-div">
+            <div className="dev-icon-div" data-aos="fade-left">
               {" "}
-              <i
-                className="devicon-sass-plain colored skill-icon"
-                data-aos="fade-left"
-              >
+              <i className="devicon-sass-plain colored skill-icon">
                 <p>Sass</p>
               </i>
             </div>
-            <div className="dev-icon-div">
+            <div className="dev-icon-div" data-aos="fade-right">
               {" "}
-              <i
-                className="devicon-tailwindcss-plain colored skill-icon"
-                data-aos="fade-right"
-              >
+              <i className="devicon-tailwindcss-plain colored skill-icon">
                 <p>Tailwind CSS</p>
               </i>
             </div>
@@ -103,64 +76,43 @@ export default function Resume() {
           <div className="m-1 my-5 backend">
             <h3>Back-End:</h3>
             <div className="home-skills-icon-cont d-flex text-center flex-wrap justify-content-between">
-              <div className="dev-icon-div">
+              <div className="dev-icon-div" data-aos="fade-left">
                 {" "}
-                <i
-                  className="devicon-nodejs-plain colored skill-icon"
-                  data-aos="fade-left"
-                >
+                <i className="devicon-nodejs-plain colored skill-icon">
                   <p>Node.js</p>
                 </i>
               </div>
-              <div className="dev-icon-div">
-                <i
-                  className="devicon-java-plain colored skill-icon"
-                  data-aos="fade-left"
-                >
+              <div className="dev-icon-div" data-aos="fade-left">
+                <i className="devicon-java-plain colored skill-icon">
                   <p>Java</p>
                 </i>
               </div>
-              <div className="dev-icon-div">
+              <div className="dev-icon-div" data-aos="fade-right">
                 {" "}
-                <i
-                  className="devicon-express-original colored skill-icon"
-                  data-aos="fade-right"
-                >
+                <i className="devicon-express-original colored skill-icon">
                   <p>Express</p>
                 </i>
               </div>
-              <div className="dev-icon-div">
+              <div className="dev-icon-div" data-aos="fade-right">
                 {" "}
-                <i
-                  className="devicon-python-plain colored skill-icon"
-                  data-aos="fade-left"
-                >
+                <i className="devicon-python-plain colored skill-icon">
                   <p>Python</p>
                 </i>
               </div>
-              <div className="dev-icon-div">
+              <div className="dev-icon-div" data-aos="fade-right">
                 {" "}
-                <i
-                  className="devicon-mongodb-plain colored skill-icon"
-                  data-aos="fade-right"
-                >
+                <i className="devicon-mongodb-plain colored skill-icon">
                   <p>MongoDB</p>
                 </i>
               </div>
-              <div className="dev-icon-div">
+              <div className="dev-icon-div" data-aos="fade-left">
                 {" "}
-                <i
-                  className="devicon-mysql-plain colored skill-icon"
-                  data-aos="fade-left"
-                >
+                <i className="devicon-mysql-plain colored skill-icon">
                   <p>MySQL</p>
                 </i>
               </div>
-              <div className="dev-icon-div">
-                <i
-                  className="devicon-graphql-plain-wordmark colored skill-icon"
-                  data-aos="fade-left"
-                >
+              <div className="dev-icon-div" data-aos="fade-left">
+                <i className="devicon-graphql-plain-wordmark colored skill-icon">
                   <p>Graph QL</p>
                 </i>
               </div>
@@ -170,38 +122,26 @@ export default function Resume() {
             <h3>DevOps:</h3>
 
             <div className="home-skills-icon-cont d-flex text-center flex-wrap justify-content-between">
-              <div className="dev-icon-div">
+              <div className="dev-icon-div" data-aos="fade-left">
                 {" "}
-                <i
-                  className="devicon-git-plain colored skill-icon"
-                  data-aos="fade-left"
-                >
+                <i className="devicon-git-plain colored skill-icon">
                   <p>Git</p>
                 </i>
               </div>
-              <div className="dev-icon-div">
+              <div className="dev-icon-div" data-aos="fade-right">
                 {" "}
-                <i
-                  className="devicon-github-original colored skill-icon"
-                  data-aos="fade-right"
-                >
+                <i className="devicon-github-original colored skill-icon">
                   <p>Github</p>
                 </i>
               </div>
-              <div className="dev-icon-div">
+              <div className="dev-icon-div" data-aos="fade-left">
                 {" "}
-                <i
-                  className="devicon-vscode-plain colored skill-icon"
-                  data-aos="fade-left"
-                >
+                <i className="devicon-vscode-plain colored skill-icon">
                   <p>VS CODE</p>
                 </i>
               </div>
-              <div className="dev-icon-div">
-                <i
-                  className="devicon-npm-original-wordmark colored skill-icon"
-                  data-aos="fade-right"
-                >
+              <div className="dev-icon-div" data-aos="fade-right">
+                <i className="devicon-npm-original-wordmark colored skill-icon">
                   <p>NPM</p>
                 </i>
               </div>
